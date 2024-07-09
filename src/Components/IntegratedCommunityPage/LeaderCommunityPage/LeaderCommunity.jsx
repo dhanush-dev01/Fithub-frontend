082c794f714a96fee68bda081c270321f4cda931
jeevan <jeevan@gmail.com>
2024-07-09 12:21:16 +0530

    No Bugs in Community Creation

--- src/Components/IntegratedCommunityPage/LeaderCommunityPage/LeaderCommunity.jsx
+++ src/Components/IntegratedCommunityPage/LeaderCommunityPage/LeaderCommunity.jsx
@@ -4,63 +4,52 @@ import './Styles/leaderCommunity.css';
 import logo from '../../../assets/Images/Joggingppl.jpg';
 import ChatHome from '../../ChatModule/ChatHome';
 import UploadProducts from './UploadProducts';
-import { FaCommentDots } from 'react-icons/fa';
 import { AuthContext } from '../../context/AuthContext';
 
 export default function LeaderCommunity() {
-  const [isFormVisible, setIsFormVisible] = useState(false);
-  const [communityName, setCommunityName] = useState('');
-  const [address, setAddress] = useState('');
-  const [agenda, setAgenda] = useState('');
-  const [location, setLocation] = useState('');
-  const [iconUrl, setIconUrl] = useState('');
-  const [communities, setCommunities] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [newCommunityName, setNewCommunityName] = useState('');
+  const [newAddress, setNewAddress] = useState('');
+  const [newAgenda, setNewAgenda] = useState('');
+  const [newLocation, setNewLocation] = useState('');
+  const [newIconUrl, setNewIconUrl] = useState('');
+  const [communityList, setCommunityList] = useState([]);
   const { currentUser } = useContext(AuthContext);
-  const [error, setError] = useState('');
-  const [isChatActive, setIsChatActive] = useState(false);
-  const [isUploadActive, setIsUploadActive] = useState(false);
-  const [communityMembers, setCommunityMembers] = useState([]);
+  const [formError, setFormError] = useState('');
+  const [chatVisible, setChatVisible] = useState(false);
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [membersList, setMembersList] = useState([]);
 
   useEffect(() => {
     if (currentUser) {
-      fetchCommunities();
+      loadCommunityMembers();
     }
   }, [currentUser]);
 
-  const fetchCommunities = async () => {
-    try {
-      const response = await axios.get(`https://machjava.azurewebsites.net/customobj/getCommunity`, {
-        params: { leaderId: currentUser.id },
-      });
-      const filteredCommunities = response.data.filter((community, index) => index >= 3);
-
-
-      // dont set here
-      // setCommunities(filteredCommunities);
-
-      if (filteredCommunities.length > 0) {
-        fetchCommunityMembers(filteredCommunities[0].name,filteredCommunities);
-      }
-    } catch (error) {
-      console.error('Error fetching communities:', error);
-    }
+  const loadCommunities = async () => {
   };
 
-  const fetchCommunityMembers = async (communityName,filteredCommunities) => {
+  const loadCommunityMembers = async () => {
     try {
-      const response = await axios.get('https://machjava.azurewebsites.net/customer/getCustomersByCommunity', {
-        params: {
-          communityName,
-        },
+      const response = await axios.get(`https://machjava.azurewebsites.net/customer/getCommunity`, {
+        params: { customerid: localStorage.getItem("customerId") },
       });
 
-     let loggedInUser = currentUser.email;
-       let array = response.data.filter(each => each.email == loggedInUser);
+      if (response.status === 200) {
+        const communityName = response.data;
+        setNewCommunityName(communityName);
 
-      // console.log("Community members response: ", response.data);
-      setCommunityMembers(array);
-      if(array.length > 0){
-        setCommunities(filteredCommunities);
+        const membersResponse = await axios.get('https://machjava.azurewebsites.net/customer/getCustomersByCommunity', {
+          params: { communityName },
+        });
+
+        if (membersResponse.status === 200) {
+          setMembersList(membersResponse.data);
+
+          const communitiesResponse = await axios.get('https://machjava.azurewebsites.net/customobj/getCommunity');
+          const myCommunity = communitiesResponse.data.filter(community => community.name === communityName);
+          setCommunityList(myCommunity);
+        }
       }
     } catch (error) {
       console.error('Error fetching community members:', error);
@@ -68,117 +57,111 @@ export default function LeaderCommunity() {
   };
 
   const handleCreateCommunityClick = () => {
-    setIsFormVisible(true);
+    setShowForm(true);
   };
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-    
-    // Validate community name for spaces
-    if (communityName.includes(' ')) {
-      setError('Community name cannot contain spaces.');
+
+    if (newCommunityName.includes(' ')) {
+      setFormError('Community name cannot contain spaces.');
       return;
     }
 
-    const newCommunity = { name: communityName, address, agenda, location, iconUrl };
+    const newCommunity = { name: newCommunityName, address: newAddress, agenda: newAgenda, location: newLocation, iconUrl: newIconUrl };
 
     try {
       const response = await axios.post('https://machjava.azurewebsites.net/customobj/addCommunity', newCommunity, {
-        params: { customerid:   localStorage.getItem("customerId")},
+        params: { customerid: localStorage.getItem("customerId") },
       });
+
       if (response.status === 200) {
-        setCommunities([...communities, response.data]);
-        setCommunityName('');
-        setAddress('');
-        setAgenda('');
-        setLocation('');
-        setIconUrl('');
-        setIsFormVisible(false);
-        fetchCommunityMembers(response.data.name); 
-        window.location.reload(); 
+        setCommunityList([...communityList, response.data]);
+        resetForm();
+        loadCommunityMembers(); 
       }
     } catch (error) {
       console.error('Error creating community:', error);
-      setError('Failed to create community. Please try again.');
+      setFormError('Failed to create community. Please try again.');
     }
-
-    setCommunityName('');
-    setLocation('');
-    setIsFormVisible(false);
   };
 
   const handleDeleteCommunity = async (communityName) => {
     try {
       const response = await axios.delete(`https://machjava.azurewebsites.net/customobj/removeCommunity?community=${encodeURIComponent(communityName)}`);
       if (response.status === 200) {
-        setCommunities(communities.filter(community => community.name !== communityName));
-      } else {
-        console.error('Failed to delete community:', response.data);
+        setCommunityList(communityList.filter(community => community.name !== communityName));
       }
     } catch (error) {
       console.error('Error deleting community:', error);
     }
   };
 
+  const resetForm = () => {
+    setNewCommunityName('');
+    setNewAddress('');
+    setNewAgenda('');
+    setNewLocation('');
+    setNewIconUrl('');
+    setShowForm(false);
+    setFormError('');
+  };
+
   const toggleChat = () => {
-    setIsChatActive(!isChatActive);
-    setIsUploadActive(false);
+    setChatVisible(!chatVisible);
+    setUploadVisible(false);
   };
 
   const toggleUpload = () => {
-    setIsUploadActive(!isUploadActive);
-    setIsChatActive(false);
+    setUploadVisible(!uploadVisible);
+    setChatVisible(false);
   };
 
   return (
     <div className="leader-community-container">
       <h1 className='styledHeading'>Your Community!</h1>
-
       <div className="CommunityPlusCommunityMembers">
-        {!isFormVisible && communities.length === 0 && (
+        {!showForm && communityList.length === 0 && (
           <button className="create-Community-button" onClick={handleCreateCommunityClick}>
             Create
           </button>
         )}
-        {!isChatActive && !isUploadActive && (
+        {!chatVisible && !uploadVisible && (
           <>
             <div className="community-list">
-              {communities.map((community, index) => (
+              {communityList.map((community, index) => (
                 <div key={index} className="community-item">
                   <div>
                     <div className="imagePlusButtons">
                       <img src={logo} alt="Logo" className="headerlogo" />
                       <div className="community-actions">
                         <button className="upload-items-button" onClick={toggleUpload}>
-                          Upload 
+                          Upload
                         </button>
-                        
                         <button className="chat-with-community-button" onClick={toggleChat}>
                           Chat with us
                         </button>
                         <button onClick={() => handleDeleteCommunity(community.name)} className="delete-Community-button">
-                          Delete 
+                          Delete
                         </button>
                       </div>
                     </div>
-            
                     <span className="community-name">{community.name} Community</span>
                     <br />
                     <span className="community-address">{community.address}</span>
                     <br />
                     <span className="community-agenda">{community.agenda}</span>
                   </div>
-                  
                 </div>
               ))}
             </div>
-            {isFormVisible && (
+            {showForm && (
               <form onSubmit={handleFormSubmit} className="community-form">
                 <div className="form-group">
                   <input
                     type="text"
-                    value={communityName}
-                    onChange={(e) => setCommunityName(e.target.value)}
+                    value={newCommunityName}
+                    onChange={(e) => setNewCommunityName(e.target.value)}
                     placeholder="Enter community name"
                     required
                   />
@@ -186,8 +169,8 @@ export default function LeaderCommunity() {
                 <div className="form-group">
                   <input
                     type="text"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
+                    value={newAddress}
+                    onChange={(e) => setNewAddress(e.target.value)}
                     placeholder="Enter address"
                     required
                   />
@@ -195,33 +178,33 @@ export default function LeaderCommunity() {
                 <div className="form-group">
                   <input
                     type="text"
-                    value={agenda}
-                    onChange={(e) => setAgenda(e.target.value)}
+                    value={newAgenda}
+                    onChange={(e) => setNewAgenda(e.target.value)}
                     placeholder="Enter agenda"
                     required
                   />
                 </div>
-                {error && <p className="error-message">{error}</p>}
+                {formError && <p className="error-message">{formError}</p>}
                 <button type="submit" className="submit-Community-button">Create</button>
               </form>
             )}
           </>
         )}
-        {isChatActive && (
+        {chatVisible && (
           <div className="chat-home-container">
             <ChatHome />
           </div>
         )}
-        {isUploadActive && (
+        {uploadVisible && (
           <div className="upload-products-container">
             <UploadProducts />
           </div>
         )}
-        {communities.length > 0 && (
+        {communityList.length > 0 && (
           <div className="community-members-container">
             <h4 className='styledHeading'>Community Members</h4>
             <ul className="list-group">
-              {communityMembers.map((member, index) => (
+              {membersList.map((member, index) => (
                 <li key={index} className="list-group-item">
                   {member.firstName} {member.lastName}
                 </li>
